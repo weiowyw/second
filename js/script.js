b7f38495178814a8323bca563f40328e5dc5cb6d
weiowyw <haxjlebatop@gmail.com>
2023-07-03 23:10:04 +0500

    yes

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+// 1.
+// const price = "$120";
+// console.log(price);
+//
+// const fixPrice = price.slice(1, price.length);
+// console.log(fixPrice);
+
+
+// 2.
+// const userStrMaxLength = prompt('Введите максимальный размер строки', );
+// const maxLength = parseInt(userStrMaxLength);
+// let userStr = prompt('Введите строку', );
+//
+// function strSlice() {
+//     if (userStr.length > userStrMaxLength) {
+//         userStr = userStr.slice(0, maxLength);
+//         userStr = userStr + "...";
+//     }
+// }
+// strSlice();
+// console.log(userStr);
+
+
+// 3.
+// const userStr = prompt('Введите строку', );
+// const result = userStr.includes('free');
+// console.log(result);
